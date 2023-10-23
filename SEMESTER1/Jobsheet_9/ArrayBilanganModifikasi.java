410d614a9e8f486c04b345893f505c5ea38c98f6
leonanda1013 <leonandaprabowo@gamil.com>
2023-10-23 11:59:13 +0700

    update

--- SEMESTER1/Jobsheet_9/ArrayBilanganModifikasi.java
+++ SEMESTER1/Jobsheet_9/ArrayBilanganModifikasi.java
@@ -2,15 +2,14 @@ package SEMESTER1.Jobsheet_9;
 
 public class ArrayBilanganModifikasi {
     public static void main(String[] args) {
-        int[] bil = new int[4];
+        double[] bil = new double[4];
         bil[0]= 5.0; //akan terjadi eror karena harusnnya double
         bil[1]= 12867;
         bil[2]= 7.5;
         bil[3]= 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for (int i = 0; i <= 4; i++) {
+         System.out.println(bil[i]); //ini merupakan perulangan untuk print bilangan pada varible bil tanpa menuliskan print satu per satu
+       }
     }
 }
--- SEMESTER1/Jobsheet_9/BubbleSortExample29.java
+++ SEMESTER1/Jobsheet_9/BubbleSortExample29.java
@@ -6,8 +6,10 @@ public class BubbleSortExample29 {
         int[] intData = {34, 18, 87, 72, 32, 54, 43}; //ini merupkan deklarasi dan inisialisasi dari array bertipe int
         int temp = 0;
         for (int i = 0; i < intData.length; i++) { //ini merupakan perulakan pertama
-            for (int j = 1; j < intData.length-i; j++) {
-                if (intData[j-1]>intData[j]) {
+            for (int j = 1; j < intData.length-i; j++) { //Setiap iterasi mengurangi jumlah elemen yang masih belum terurut sebanyak 1 (karena elemen terbesar akan "mengapung" ke posisinya yang benar setiap kali iterasi).
+                
+            
+                if (intData[j-1]>intData[j]) { //digunakan untuk membandingkan pasangan elemen berturut-turut dan menukar mereka jika elemen sebelumnya lebih besar dari elemen saat ini. Ini adalah langkah inti dari algoritma pengurutan gelembung.
                     //swap elemen
                     temp = intData[j];
                     intData[j]=intData[j-1];
